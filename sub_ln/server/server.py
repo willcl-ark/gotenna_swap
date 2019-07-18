564bcd8b6dab8fde55ec1c9589994c5a7d4db022
willcl-ark <will8clark@gmail.com>
2019-07-18 14:40:17 +0100

    Server starting will auto-init the db
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- sub_ln/server/server.py
+++ sub_ln/server/server.py
@@ -1,27 +1,30 @@
-# import logging
 from flask import Flask
 from flask_restful import Api
 
-from sub_ln.api.api import BumpSatOrder, CheckRefundAddress, CheckSwap, CreateOrder, CreateSwap, \
-    ExecuteSwap, GetRefundAddress, LookupInvoice, Rand64ByteMsg
-
-# logger = logging.getLogger('API')
-# FORMAT = "[%(asctime)s - %(levelname)8s - %(funcName)20s() ] - %(message)s"
-# logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+from sub_ln.api.api import BlocksatBump, SwapCheckRefundAddress, SwapCheck, CreateOrder, SwapQuote, \
+    SwapPay, GetRefundAddress, SwapLookupInvoice, Rand64ByteMsg
+from sub_ln.database import db
 
 
+# setup the Flask app
 app = Flask(__name__)
 app.config["DEBUG"] = True
 api = Api(app)
 
+# add the API endpoints
 api.add_resource(Rand64ByteMsg, '/api/v1/util/random_message')
-api.add_resource(LookupInvoice, '/api/v1/swap/lookup_invoice')
-api.add_resource(CheckRefundAddress, '/api/v1/swap/check_refund_address')
-api.add_resource(CreateOrder, '/api/v1/blocksat/create_order')
-api.add_resource(BumpSatOrder, '/api/v1/blocksat/bump_order')
-api.add_resource(GetRefundAddress, '/api/v1/bitcoin/get_new_address')
-api.add_resource(CreateSwap, '/api/v1/swap/create_swap')
-api.add_resource(ExecuteSwap, '/api/v1/swap/execute_swap')
-api.add_resource(CheckSwap, '/api/v1/swap/check_swap')
+api.add_resource(SwapLookupInvoice, '/api/v1/swap/lookup_invoice')
+api.add_resource(SwapCheckRefundAddress, '/api/v1/swap/check_refund_addr')
+api.add_resource(CreateOrder, '/api/v1/order/create')
+api.add_resource(BlocksatBump, '/api/v1/blocksat/bump')
+api.add_resource(GetRefundAddress, '/api/v1/bitcoin/new_address')
+api.add_resource(SwapQuote, '/api/v1/swap/quote')
+api.add_resource(SwapPay, '/api/v1/swap/pay')
+api.add_resource(SwapCheck, '/api/v1/swap/check')
 
+# initialise the db, this will check for presence of tables before creating, so safe
+# to call multiple times
+db.init()
+
+# start the API server
 app.run()
