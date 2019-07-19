5493423353faad80a5a143ecf0487576bfaf4a73
willcl-ark <will8clark@gmail.com>
2019-07-19 15:40:36 +0100

    Updated pipfile to reflect requirements properly
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ import setuptools
 
 setuptools.setup(
     name="go-sat-sub",
-    version="0.0.1",
+    version="0.0.2",
     author="Will Clark",
     author_email="will8clark@gmail.com",
     description="upload a message to Blockstream Blocksat, paying lightning invoice using a"
                 " submarine swap",
     # long_description=long_description,
     # long_description_content_type="text/markdown",
-    url="https://github.com/willcl-ark/blockstream_blocksat",
+    url="https://github.com/willcl-ark/go-sat-sub",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     keywords="gotenna submarine blockstream satellite swap",
     install_requires=[],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
