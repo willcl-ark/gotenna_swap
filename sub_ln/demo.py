7a7bedd88ba270dd34d7b0c82e05d1ee286110a8
willcl-ark <will8clark@gmail.com>
2019-07-24 12:29:53 +0100

    Improved debug loggin
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- sub_ln/demo.py
+++ sub_ln/demo.py
@@ -1,9 +1,11 @@
 import logging
-import time
-from json.decoder import JSONDecodeError
-
 import requests
-from blocksat_api.blocksat import TESTNET_SATELLITE_API
+import time
+
+from json.decoder import JSONDecodeError
+from pprint import pformat
+
+from sub_ln.utilities import clock
 
 """
 A run-through of an API workflow
@@ -14,38 +16,30 @@ URL = "http://127.0.0.1:5000/api/v1/"
 SATOSHIS = 100_000_000
 
 logger = logging.getLogger(__name__)
-FORMAT = (
-    "[%(asctime)s - %(levelname)8s - %(filename)22s - %(funcName)10s() ] - %(message)s"
-)
+FORMAT = "[%(asctime)s - %(levelname)s] - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 
 def main():
-
-    # while True:
 
     # create the request session
     s = requests.Session()
 
     # create a random message for testing
     message = s.get(URL + "util/random_message").json()
-    logger.debug(f"Created random message: {message}")
+    logger.debug(f"Created random message:\n{pformat(message)}")
 
     # create the blocksat order
-    order_params = {
-        "message": message["message"],
-        "bid": 10000,
-        "satellite_url": TESTNET_SATELLITE_API,
-        "network": "testnet",
-    }
+    order_params = {"message": message["message"], "bid": 10000, "network": "testnet"}
     blocksat_order = s.post(URL + "order/create", json=order_params)
     if blocksat_order.status_code == 200:
         blocksat_order = blocksat_order.json()
         logger.debug(
-            f"Created order {blocksat_order['uuid']} for blocksat order: {blocksat_order['order']}"
+            f"Created order: {blocksat_order['uuid']} for blocksat order:\n{pformat(blocksat_order['order'])}"
         )
     else:
         logger.error(f"Failed to setup blocksat order")
+        return
 
     # set our order uuid to one returned by API (not blocksat_order.blocksat_uuid though!)
     uuid = blocksat_order["uuid"]
@@ -69,7 +63,8 @@ def main():
         invoice_lookup = invoice_lookup.json()
         logger.debug(f"Successfully looked up invoice with swap server")
     else:
-        logger.error(f"Failed invoice lookup with swap server")
+        logger.error(f"Failed invoice lookup with swap server\n{pformat({'code': invoice_lookup.status_code, 'text': invoice_lookup.text})}")
+        return
 
     time.sleep(2)
     # Get a refund address for the swap
@@ -82,6 +77,7 @@ def main():
         )
     else:
         logger.error(f"Failed to get a refund address")
+        return
 
     time.sleep(2)
     # create the swap with swap server
@@ -94,15 +90,18 @@ def main():
     create_swap = s.post(URL + "swap/quote", json=create_swap_params)
     if create_swap.status_code == 200:
         create_swap = create_swap.json()
-        logging.debug(f"Sucessfully created the swap request with swap server")
+        logging.debug(f"Successfully created the swap request with swap server:\n"
+                      f"{pformat(create_swap)}")
     else:
         logging.error(f"Failed to setup the swap request with the server")
+        return
 
     time.sleep(10)
     # pay on-chain swap payment
-    swap_amt_bitcoin = create_swap["swap"]["swap_amount"] / SATOSHIS
     pay_swap_params = {"uuid": uuid}
-    pay_swap = s.post(URL + "swap/pay", json=pay_swap_params).json()
+    pay_swap = s.post(URL + "swap/pay", json=pay_swap_params)
+    logging.debug(f"pay_swap: {pformat(pay_swap)}")
+    pay_swap = pay_swap.json()
     if "txid" in pay_swap:
         logger.debug(
             f"Successfully executed on-chain payment for swap, txid: {pay_swap['txid']}"
@@ -118,9 +117,7 @@ def main():
 
     while tries < 10:
         try:
-            swap_status = s.get(
-                URL + "swap/check_swap", json=swap_status_params
-            ).json()
+            swap_status = s.get(URL + "swap/check_swap", json=swap_status_params).json()
         except JSONDecodeError as e:
             print("Ouch")
             logger.debug(e)
